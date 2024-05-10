226d530ec283dfb1e30ab4e826da52e0078178b2
namxxd <lethiennam111@gmail.com>
2024-05-10 13:09:04 +0200

    xd

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://lets-go-phi.vercel.app";
 
 export function convertDate(timestamp: string) : string {
   const date = new Date(timestamp);
